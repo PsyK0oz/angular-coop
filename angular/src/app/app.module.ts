5138c438ce59548fefe07b3e67520fac01d485bb
PsyK0oz <98573684+PsyK0oz@users.noreply.github.com>
2023-12-28 18:28:20 +0100

    accueil + carousel component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CarrouselComponent } from './accueil/carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule,
