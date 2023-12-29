068c2cac10de5676ffbfc372a88b856cac5e824b
PsyK0oz <98573684+PsyK0oz@users.noreply.github.com>
2023-12-29 11:17:49 +0100

    fixed home page et formulaire on different pages
    il faut encore faire le formulaire en reactif

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormulaireComponent } from './formulaire/formulaire.component';
-
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
-  { path: 'formulaire', component: FormulaireComponent }
-
+  { path: '', component: AccueilComponent, pathMatch: 'full' }, // Route d'accueil
+  { path: 'formulaire', component: FormulaireComponent }, // Route du formulaire
+  // autres routes...
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-    
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
