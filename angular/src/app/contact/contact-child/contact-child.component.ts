f1673f652fe72ab026e78b5d81f8335c675e43f3
PsyK0oz <98573684+PsyK0oz@users.noreply.github.com>
2023-12-31 17:01:31 +0100

    contact dynamique en marche
    cela fonctionne

--- angular/src/app/contact/contact-child/contact-child.component.ts
+++ angular/src/app/contact/contact-child/contact-child.component.ts
@@ -1,5 +1,6 @@
 // contact-child.component.ts
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 interface Contact {
   name: string;
@@ -11,13 +12,26 @@ interface Contact {
 @Component({
   selector: 'app-contact-child',
   templateUrl: './contact-child.component.html',
-  styleUrls: ['./contact-child.component.scss']
+  styleUrls: ['./contact-child.component.scss'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ContactChildComponent {
   @Input() contact?: Contact;
-  showDetails = false;
+  state = 'collapsed';
 
-  toggleDetails() {
-    this.showDetails = !this.showDetails;
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.state = 'expanded';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.state = 'collapsed';
   }
 }
